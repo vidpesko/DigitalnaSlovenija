0dfffe1efe811ee46f3b3d2375651d832ef30167
Vid Pesko <vid@pesko.si>
2025-01-25 01:18:08 +0100

    Initial code commit

---
+++ DigitalnaSlovenija/src/lib/components/ToolCard.svelte
@@ -0,0 +1,22 @@
+<script>
+    import MaterialSymbolsAlarm from '~icons/material-symbols/alarm'
+
+    let { btnText="Poglej podrobno", title, description, btnLink, comingSoon=false } = $props();
+</script>
+
+<div class="rounded-md p-4 bg-stone-300 dark:bg-stone-800 hover:border-stone-300 border-stone-500 border transition-all text-center flex flex-col justify-between {comingSoon ? "tooltip" : ""} tooltip-bottom tooltip-error" data-tip="Ta funkcije je še v razvoju 🥺. Dodana bo v prihodnjih dneh.">
+    <div class="">
+        <!-- Title -->
+        <h4 class="text-center font-semibold ">{title}</h4>
+        <!-- Description -->
+        <p class="text-sm text-start">{description}</p>
+    </div>
+    <!-- CTA -->
+    <button class="btn {comingSoon ? "btn-disabled" : "btn-primary"} mt-4">
+        {#if !comingSoon}
+        {btnText}
+        {:else}
+        V razvoju <MaterialSymbolsAlarm class="text-lg" />
+        {/if}
+    </button>
+</div>
---
+++ DigitalnaSlovenija/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import ToolCard from "$lib/components/ToolCard.svelte";
+
+    import projects from "$lib/projects.json";
+</script>
+
+
+<div class="px-24">
+    <!-- Title, description, contact -->
+    <div class="text-center mt-24 mb-16">
+        <h1 class="font-bold text-4xl">Digitalna Slovenija</h1>
+        <p>Zbirka orodij za pomoč razvijalcem. Od API za znane spletne strani do različnih pripomočkov za vsakodnevno življenje.</p>
+    </div>
+    
+    <!-- Tools -->
+    <div class="mx-auto max-w-[80rem]">
+        {#each [false, true] as comingSoonAllowed}
+        <h3 class="mb-2 text-lg font-medium">
+            {#if comingSoonAllowed}
+            Prihaja kmalu
+            {:else}
+            Izdelki
+            {/if}
+        </h3>
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 mb-8">
+            {#each projects.filter((project) => project.comingSoon == comingSoonAllowed) as project}
+            <ToolCard {...project} />
+            {/each}
+        </div>
+        <div class="divider"></div>
+        {/each}
+    </div>
+</div>
