f0a0a55be58db598fe8d8b1e4a39eff458973576
Vid Pesko <vid@pesko.si>
2025-01-29 23:48:03 +0100

    Finished filter fields on create reminder page

--- DigitalnaSlovenija/src/routes/avtonet-obvescanje/ustvari/+page.svelte
+++ DigitalnaSlovenija/src/routes/avtonet-obvescanje/ustvari/+page.svelte
@@ -1,6 +1,109 @@
 <script>
     import MaterialSymbolsMailRounded from "~icons/material-symbols/mail-rounded";
+    import MaterialSymbolsCalendarMonthRounded from '~icons/material-symbols/calendar-month-rounded';
+    import MaterialSymbolsCarTag from '~icons/material-symbols/car-tag';
+    import MaterialSymbolsAttachMoneyRounded from '~icons/material-symbols/attach-money-rounded';
+    import MaterialSymbolsSpeedOutlineRounded from '~icons/material-symbols/speed-outline-rounded';
+
+    const fields = [
+        {
+            title: "Znamka, model",
+            icon: MaterialSymbolsCarTag,
+            input: inlineInputs,
+            inputInfo: [
+                {
+                    title: "Znamka",
+                    placeholder: "Audi, BMW,...",
+                    type: "text"
+                },
+                {
+                    title: "Model",
+                    placeholder: "318d, A6,...",
+                    type: "text"
+                },
+            ],
+        },
+        {
+            title: "Cena",
+            icon: MaterialSymbolsAttachMoneyRounded,
+            input: inlineInputs,
+            inputInfo: [
+                {
+                    title: "Min cena",
+                    placeholder: "",
+                    type: "number"
+                },
+                {
+                    title: "Max cena",
+                    placeholder: "",
+                    type: "number"
+                }
+            ]
+        },
+        {
+            title: "Letnik 1. registracije",
+            icon: MaterialSymbolsCalendarMonthRounded,
+            input: inlineInputs,
+            inputInfo: [
+                {
+                    title: "Min letnik 1. registracije",
+                    placeholder: "2014, 2016,...",
+                    type: "number"
+                },
+                {
+                    title: "Max letnik 1. registracije",
+                    placeholder: "2018, 2019,...",
+                    type: "number"
+                }
+            ]
+        },
+        {
+            title: "Kilometrina",
+            icon: MaterialSymbolsSpeedOutlineRounded,
+            input: inlineInputs,
+            inputInfo: [
+                {
+                    title: "Min kilometri",
+                    placeholder: "1000, 100000,...",
+                    type: "number"
+                },
+                {
+                    title: "Max kilometri",
+                    placeholder: "50000, 100000,...",
+                    type: "number"
+                }
+            ]
+        },
+    ];
 </script>
+
+<!-- Inputs -->
+{#snippet inlineInputs(inputs)}
+    {#each inputs as input}
+        <label class="form-control w-full max-w-xs mb-4">
+            <div class="label">
+                <span class="label-text">{input.title}</span>
+            </div>
+            <input
+                type={input.type}
+                placeholder={input.placeholder}
+                class="input input-bordered w-full max-w-xs"
+            />
+        </label>
+    {/each}
+{/snippet}
+
+{#snippet fieldContainer(fieldInfo)}
+    <div class="collapse collapse-arrow join-item border-base-300 border">
+        <input type="radio" name="my-accordion-4" />
+        <div class="collapse-title text-xl font-medium flex items-center gap-x-4"><svelte:component this={fieldInfo.icon} /> {fieldInfo.title}</div>
+        <!-- Inputs -->
+        <div class="collapse-content flex gap-x-4">
+            {@render fieldInfo.input(fieldInfo.inputInfo)}
+        </div>
+    </div>
+{/snippet}
+
 
 <div class="content-container">
     <!-- Navigation, title, description -->
@@ -14,7 +117,7 @@
     <h1 class="text-4xl font-semibold">Ustvari opomnik</h1>
 
     <!-- Form -->
-    <form action="" class="mt-16">
+    <form action="" class="my-16">
         <!-- Email -->
         <label class="form-control w-full max-w-xs mb-4">
             <div class="label">
@@ -30,63 +133,9 @@
         <!-- Filters -->
         <p class="mb-2 label-text">Izberite filtre:</p>
         <div class="join join-vertical w-full">
-            <!-- Brand, make -->
-            <div
-                class="collapse collapse-arrow join-item border-base-300 border"
-            >
-                <input type="radio" name="my-accordion-4" checked="checked" />
-                <div class="collapse-title text-xl font-medium">
-                    Znamka, model
-                </div>
-                <div class="collapse-content flex gap-x-4">
-                    <label class="form-control w-full max-w-xs mb-4">
-                        <div class="label">
-                            <span class="label-text"
-                                >Znamka</span
-                            >
-                        </div>
-                        <input
-                            type="text"
-                            placeholder="Audi, BMW,..."
-                            class="input input-bordered w-full max-w-xs"
-                        />
-                    </label>
-                    <label class="form-control w-full max-w-xs mb-4">
-                        <div class="label">
-                            <span class="label-text"
-                                >Model</span
-                            >
-                        </div>
-                        <input
-                            type="text"
-                            placeholder="Audi, BMW,..."
-                            class="input input-bordered w-full max-w-xs"
-                        />
-                    </label>
-                </div>
-            </div>
-            <div
-                class="collapse collapse-arrow join-item border-base-300 border"
-            >
-                <input type="radio" name="my-accordion-4" />
-                <div class="collapse-title text-xl font-medium">
-                    Click to open this one and close others
-                </div>
-                <div class="collapse-content">
-                    <p>hello</p>
-                </div>
-            </div>
-            <div
-                class="collapse collapse-arrow join-item border-base-300 border"
-            >
-                <input type="radio" name="my-accordion-4" />
-                <div class="collapse-title text-xl font-medium">
-                    Click to open this one and close others
-                </div>
-                <div class="collapse-content">
-                    <p>hello</p>
-                </div>
-            </div>
+            {#each fields as field}
+                {@render fieldContainer(field)}
+            {/each}
         </div>
         <!-- Submit -->
         <button type="submit" class="btn btn-primary mt-6"
