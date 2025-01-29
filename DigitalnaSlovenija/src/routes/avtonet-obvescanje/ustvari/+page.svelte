47e49be55b7cdd3fa390c03a24fbd9e47bfedb44
Vid Pesko <vid@pesko.si>
2025-01-30 00:56:34 +0100

    Added create reminder confirmation modal

--- DigitalnaSlovenija/src/routes/avtonet-obvescanje/ustvari/+page.svelte
+++ DigitalnaSlovenija/src/routes/avtonet-obvescanje/ustvari/+page.svelte
@@ -1,9 +1,9 @@
 <script>
     import MaterialSymbolsMailRounded from "~icons/material-symbols/mail-rounded";
-    import MaterialSymbolsCalendarMonthRounded from '~icons/material-symbols/calendar-month-rounded';
-    import MaterialSymbolsCarTag from '~icons/material-symbols/car-tag';
-    import MaterialSymbolsAttachMoneyRounded from '~icons/material-symbols/attach-money-rounded';
-    import MaterialSymbolsSpeedOutlineRounded from '~icons/material-symbols/speed-outline-rounded';
+    import MaterialSymbolsCalendarMonthRounded from "~icons/material-symbols/calendar-month-rounded";
+    import MaterialSymbolsCarTag from "~icons/material-symbols/car-tag";
+    import MaterialSymbolsAttachMoneyRounded from "~icons/material-symbols/attach-money-rounded";
+    import MaterialSymbolsSpeedOutlineRounded from "~icons/material-symbols/speed-outline-rounded";
 
     const fields = [
         {
@@ -14,12 +14,14 @@
                 {
                     title: "Znamka",
                     placeholder: "Audi, BMW,...",
-                    type: "text"
+                    type: "text",
+                    name: "brand",
                 },
                 {
                     title: "Model",
                     placeholder: "318d, A6,...",
-                    type: "text"
+                    type: "text",
+                    name: "model",
                 },
             ],
         },
@@ -31,14 +33,16 @@
                 {
                     title: "Min cena",
                     placeholder: "",
-                    type: "number"
+                    type: "number",
+                    name: "min_price",
                 },
                 {
                     title: "Max cena",
                     placeholder: "",
-                    type: "number"
-                }
-            ]
+                    type: "number",
+                    name: "max_price",
+                },
+            ],
         },
         {
             title: "Letnik 1. registracije",
@@ -48,14 +52,16 @@
                 {
                     title: "Min letnik 1. registracije",
                     placeholder: "2014, 2016,...",
-                    type: "number"
+                    type: "number",
+                    name: "min_year",
                 },
                 {
                     title: "Max letnik 1. registracije",
                     placeholder: "2018, 2019,...",
-                    type: "number"
-                }
-            ]
+                    type: "number",
+                    name: "max_year",
+                },
+            ],
         },
         {
             title: "Kilometrina",
@@ -65,16 +71,36 @@
                 {
                     title: "Min kilometri",
                     placeholder: "1000, 100000,...",
-                    type: "number"
+                    type: "number",
+                    name: "min_mileage",
                 },
                 {
                     title: "Max kilometri",
                     placeholder: "50000, 100000,...",
-                    type: "number"
-                }
-            ]
+                    type: "number",
+                    name: "max_mileage",
+                },
+            ],
         },
     ];
+
+    let reminderForm;
+    let reminderFormData = {};
+    let reminderFormError;
+
+    function updateFormData() {
+        reminderFormData = {}; // Reset before updating
+        for (let el of reminderForm.elements) {
+            if (el.name) {
+                reminderFormData[el.name] = el.value;
+            }
+        }
+    }
+
+    function openModal() {
+        updateFormData();
+        document.getElementById("confirmationModal").showModal();
+    }
 </script>
 
 <!-- Inputs -->
@@ -86,6 +112,7 @@
             </div>
             <input
                 type={input.type}
+                name={input.name}
                 placeholder={input.placeholder}
                 class="input input-bordered w-full max-w-xs"
             />
@@ -95,15 +122,19 @@
 
 {#snippet fieldContainer(fieldInfo)}
     <div class="collapse collapse-arrow join-item border-base-300 border">
-        <input type="radio" name="my-accordion-4" />
-        <div class="collapse-title text-xl font-medium flex items-center gap-x-4"><svelte:component this={fieldInfo.icon} /> {fieldInfo.title}</div>
+        <input type="radio" name="filter-accordion" />
+        <div
+            class="collapse-title text-xl font-medium flex items-center gap-x-4"
+        >
+            <svelte:component this={fieldInfo.icon} />
+            {fieldInfo.title}
+        </div>
         <!-- Inputs -->
         <div class="collapse-content flex gap-x-4">
             {@render fieldInfo.input(fieldInfo.inputInfo)}
         </div>
     </div>
 {/snippet}
-
 
 <div class="content-container">
     <!-- Navigation, title, description -->
@@ -117,7 +148,7 @@
     <h1 class="text-4xl font-semibold">Ustvari opomnik</h1>
 
     <!-- Form -->
-    <form action="" class="my-16">
+    <form bind:this={reminderForm} action="" class="my-16">
         <!-- Email -->
         <label class="form-control w-full max-w-xs mb-4">
             <div class="label">
@@ -125,6 +156,7 @@
             </div>
             <input
                 type="text"
+                name="email"
                 placeholder="Email za obveščanje"
                 class="input input-bordered w-full max-w-xs"
                 value="trenutni@mail.com"
@@ -137,9 +169,55 @@
                 {@render fieldContainer(field)}
             {/each}
         </div>
-        <!-- Submit -->
-        <button type="submit" class="btn btn-primary mt-6"
-            >Ustvari opomnik</button
+        <!-- Open confirmation page -->
+        <button
+            onclick={() => {
+                if (reminderForm.checkValidity()) {
+                    openModal();
+                    reminderFormError = false;
+                } else {
+                    reminderFormError = true;
+                }
+            }}
+            class="btn btn-primary mt-6">Ustvari opomnik</button
         >
+        {#if reminderFormError}
+            <div class="toast toast-bottom toast-end">
+                <button
+                    class="alert alert-error"
+                    onclick={() => (reminderFormError = false)}
+                >
+                    <span
+                        >Napaka v obrazcu. Ali ste vnesli pravilne podatke?</span
+                    >
+                </button>
+            </div>
+        {/if}
     </form>
+
+    <dialog id="confirmationModal" class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+            <h3 class="text-lg font-bold mb-4">
+                Še zadnji korak preden dodate nov opomnik
+            </h3>
+            {#if reminderForm}
+                <p>Preverite vaše podatke:</p>
+                <ul class="list-disc list-inside">
+                    {#each Object.entries(reminderFormData) as [name, value]}
+                    {#if value && name != "filter-accordion"}
+                        <li>{name}: {value}</li>
+                    {/if}
+                    {/each}
+                </ul>
+                <p class="my-4">Vozila s izbranimi filtri:</p>
+            {/if}
+            <div class="modal-action">
+                <button class="btn btn-primary">Ustvari</button>
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn">Prekliči</button>
+                </form>
+            </div>
+        </div>
+    </dialog>
 </div>
